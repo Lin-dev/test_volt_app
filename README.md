a3afd50fe796f6b4ab7f21718553951b24fe0d9f
bklynate <nathanielcfa@gmail.com>
2016-02-19 23:51:49 -0500

    add more controllers, a model, and changed todo view html

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ## MAKE SURE YOU HAVE MONGODB INSTALLED TO RUN LOCALLY
 
 ## Followed from this tutorial --> https://www.youtube.com/watch?v=KbFtIt7-ge8&feature=youtu.be
+
--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -1,19 +1,50 @@
 # By default Volt generates this controller for your Main component
 module Main
   class MainController < Volt::ModelController
-    model :page
+    model :store
+
     def add_todo
-      _todos << page._new_todo.to_h
-      page._new_todo._label = ""
+      page._todos << { name: page._new_todo }
+      page._new_todo = ""
     end
 
-    def index
-      # Add code for when the index view is loaded
+    def current_todo
+      _todos[(params._index || 0).to_i]
     end
 
-    def about
-      # Add code for when the about view is loaded
+    def check_all
+      _todos.each { |todo| todo._completed = true }
     end
+
+    def completed
+      _todos.count { |todo| todo._completed }
+    end
+
+  def incomplete
+    # because .size and completed both return promises, we need to
+    # call .then on them to get their value.
+    _todos.size.then do |size|
+      completed.then do |completed|
+        size - completed
+      end
+    end
+  end
+
+  def percent_complete
+    _todos.size.then do |size|
+      completed.then do |completed|
+        (completed / size.to_f * 100).round
+      end
+    end
+  end
+
+    # def index
+    #   # Add code for when the index view is loaded
+    # end
+
+    # def about
+    #   # Add code for when the about view is loaded
+    # end
 
     private
 
